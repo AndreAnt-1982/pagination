cbe094ed6757e6db2f65aa63815b2ba898d0d57a
Andrii Antoniuk <61471495+AndreAnt-1982@users.noreply.github.com>
2021-09-26 00:17:04 +0100

    Initial commit

---
+++ src/app/components/users.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import User from "./user";
+import Pagination from "./pagination";
+import {paginate} from "../utils/paginate";
+const Users = ({ users:allUsers, ...rest }) => {
+    const count = allUsers.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+const handlePageChange = (pageIndex) => {
+    console.log("page: ", pageIndex);
+    setCurrentPage(pageIndex);
+}
+const users = paginate(allUsers, currentPage, pageSize);
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <User {...rest} {...user} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination 
+                itemsCount={count} 
+                pageSize={pageSize} 
+                currentPage={currentPage} 
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+
+export default Users;
